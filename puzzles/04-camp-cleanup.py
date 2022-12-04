72e0d2ddfc868f9b0ee64a3851e96c3d1aa18d53
gregkluska <kluskagreg@gmail.com>
2022-12-04 12:37:15 +0000

    add comment

--- puzzles/04-camp-cleanup.py
+++ puzzles/04-camp-cleanup.py
@@ -3,6 +3,14 @@ import util
 
 # --- Day 4: Camp Cleanup ---
 
+# Some of the pairs have noticed that one of their assignments fully contains the other.
+# For example, 2-8 fully contains 3-7, and 6-6 is fully contained by 4-6.
+# In pairs where one assignment fully contains the other,
+# one Elf in the pair would be exclusively cleaning sections their partner will already be cleaning,
+# so these seem like the most in need of reconsideration. In this example, there are 2 such pairs.
+
+# In how many assignment pairs does one range fully contain the other?
+
 def task_camp_cleanup_part_1():
     pairs = 0
 
@@ -22,6 +30,9 @@ def task_camp_cleanup_part_1():
     util.read_lines("../resources/04-input.txt", process_line)
     print(pairs)
 
+
+# It seems like there is still quite a bit of duplicate work planned.
+# Instead, the Elves would like to know the number of pairs that overlap at all.
 
 def task_camp_cleanup_part_2():
     pairs = 0
