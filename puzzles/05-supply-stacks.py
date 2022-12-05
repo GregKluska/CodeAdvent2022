a5327067369a296d8da610d0898fadfb46a0f19d
gregkluska <kluskagreg@gmail.com>
2022-12-05 12:33:01 +0000

    reuse code

--- puzzles/05-supply-stacks.py
+++ puzzles/05-supply-stacks.py
@@ -13,9 +13,7 @@ import util
 # procedure, but they forgot to ask her which crate will end up where,
 # and they want to be ready to unload them as soon as possible, so they can embark.
 
-# one at time
-
-def task_supply_stack_part_1():
+def task_supply_stack(part):
     reading_stacks = True
     stacks = []
 
@@ -39,7 +37,7 @@ def task_supply_stack_part_1():
             stacks[i].append(content)
 
     def process_instruction_line(line):
-        nonlocal stacks
+        nonlocal stacks, part
 
         instructions = line.split(" ")
         move_count = int(instructions[1])
@@ -47,7 +45,10 @@ def task_supply_stack_part_1():
         move_to = int(instructions[5]) - 1
 
         for idx in range(0, move_count):
-            stacks[move_to].insert(0, stacks[move_from].pop(0))
+            if part == 2:
+                stacks[move_to].insert(idx, stacks[move_from].pop(0))
+            else:
+                stacks[move_to].insert(0, stacks[move_from].pop(0))
 
     def is_instruction(line):
         return line.startswith("move")
@@ -66,6 +67,12 @@ def task_supply_stack_part_1():
     for i in range(0, len(stacks)):
         message = message + stacks[i][0]
     print(message)
+
+
+# one at time
+
+def task_supply_stack_part_1():
+    task_supply_stack(1)
 
 
 # The CrateMover 9001 is notable for many new and exciting
@@ -74,56 +81,7 @@ def task_supply_stack_part_1():
 # multiple crates at once.
 
 def task_supply_stack_part_2():
-    reading_stacks = True
-    stacks = []
-
-    def process_stacks_line(line):
-        nonlocal reading_stacks, stacks
-
-        if line[1] == "1":
-            reading_stacks = False
-            return
-
-        crates = [line[i:i + 4] for i in range(0, len(line), 4)]
-
-        if len(stacks) == 0:  # stacks not initialised
-            for i in range(0, len(crates)):
-                stacks.append([])
-
-        for i in range(0, len(crates)):
-            content = crates[i][1]
-            if content == " ":
-                continue
-            stacks[i].append(content)
-
-    def process_instruction_line(line):
-        nonlocal stacks
-
-        instructions = line.split(" ")
-        move_count = int(instructions[1])
-        move_from = int(instructions[3]) - 1
-        move_to = int(instructions[5]) - 1
-
-        for idx in range(0, move_count):
-            stacks[move_to].insert(idx, stacks[move_from].pop(0))
-
-    def is_instruction(line):
-        return line.startswith("move")
-
-    def process_line(line):
-        nonlocal reading_stacks
-
-        if reading_stacks:
-            process_stacks_line(line)
-        elif is_instruction(line):
-            process_instruction_line(line)
-
-    util.read_lines("../resources/05-input.txt", process_line)
-
-    message = ""
-    for i in range(0, len(stacks)):
-        message = message + stacks[i][0]
-    print(message)
+    task_supply_stack(2)
 
 
 print("--- Part 1 ---")
