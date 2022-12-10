3e5e368b7d8cece43057a3c3b1c319b1570adb6f
Greg Kluska <gregkluska@gmail.com>
2022-12-10 18:11:46 +0000

    add day 10 p2

--- puzzles/10-cathode-ray-tube.py
+++ puzzles/10-cathode-ray-tube.py
@@ -38,12 +38,75 @@ def task_cathode_ray_tube_part_1():
                 noop()
 
     util.read_lines("../resources/10-input.txt", process_line)
-    print(sum_of_signals)
+    print("The sum of these signal strengths is %s." % str(sum_of_signals))
 
-# def task_cathode_ray_tube_part_2():
+
+def task_cathode_ray_tube_part_2():
+    cycles = 1
+    x = 1
+    row = ""
+    crt = []
+
+    def render():
+        nonlocal row, x
+
+        if len(row) + 1 in range(x, x + 3):
+            row += "#"
+        else:
+            row += "."
+
+    def cycle_updated():
+        nonlocal cycles, x, row, crt
+
+        render()
+
+        if cycles % 40 == 0:
+            crt.append(row)
+            row = ""
+
+    def addx(number: int):
+        nonlocal cycles, x
+        for i in range(0, 2):
+            cycles += 1
+            if i == 1:
+                x += number
+                # print_sprint()
+            cycle_updated()
+
+    def noop():
+        nonlocal cycles
+        cycles += 1
+        cycle_updated()
+
+    def print_sprint():
+        nonlocal x
+        debug_text = "Sprite position: "
+        for pixel in range(1, 41):
+            if pixel in range(x, x + 3):
+                debug_text += "#"
+            else:
+                debug_text += "."
+        print(debug_text)
+
+    def process_line(line):
+        command = line.strip().split()
+        match command[0]:
+            case "addx":
+                addx(int(command[1]))
+            case "noop":
+                noop()
+
+    render()
+    # print_sprint()
+    util.read_lines("../resources/10-input.txt", process_line)
+
+    print()
+    print("result: ")
+    for i in crt:
+        print(i)
 
 
 print("--- Part 1 ---")
 task_cathode_ray_tube_part_1()
 print("--- Part 2 ---")
-# task_cathode_ray_tube_part_2()
+task_cathode_ray_tube_part_2()
